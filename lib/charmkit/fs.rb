afa569203c229b1abf409859d43fd9045cdeb2e9
Adam Stokes <battlemidget@users.noreply.github.com>
2016-11-29 16:30:01 -0500

    readd file hook
    Signed-off-by: Adam Stokes <battlemidget@users.noreply.github.com>

--- lib/charmkit/fs.rb
+++ lib/charmkit/fs.rb
@@ -5,16 +5,16 @@ module Charmkit
     FileUtils.singleton_methods.each do |m|
       define_method m, FileUtils.method(m).to_proc
     end
+    def file(dst, content:)
+      File.write(dst, content)
+    end
     def is_file?(path)
       return File.exists? path
     end
     def is_dir?(path)
       return Dir.exists? path
     end
-    def spew(dst, content)
-      File.write(dst, content)
-    end
-    def slurp(src)
+    def cat(src)
       return File.read(src)
     end
   end
