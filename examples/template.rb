55bb199e785326c1107f8957bd5e894fd857b0c8
Adam Stokes <battlemidget@users.noreply.github.com>
2016-11-29 07:59:02 -0500

    add template
    Signed-off-by: Adam Stokes <battlemidget@users.noreply.github.com>

---
+++ examples/template.rb
@@ -0,0 +1,3 @@
+require 'charmkit'
+
+template('templates/user_auth.txt', '/tmp/user_auth.txt', firstname: "joe", lastname: "bob")
--- lib/charmkit/template.rb
+++ lib/charmkit/template.rb
@@ -1,8 +1,21 @@
-# Hook tools module for exposing things such as status-set and action-set
+require 'erb'
+
 module Charmkit
   module DSL
-    def template(src, dst, context)
-      puts "template"
+    class TemplateRenderer
+      def self.empty_binding
+        binding
+      end
+
+      def self.render(template_content, locals = {})
+        b = empty_binding
+        locals.each { |k, v| b.local_variable_set(k, v) }
+        ERB.new(template_content).result(b)
+      end
+    end
+    def template(src, dst, **context)
+      rendered = TemplateRenderer.render(File.read(src), context)
+      File.write(dst, rendered)
     end
   end
 end
