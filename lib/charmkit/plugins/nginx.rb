b513c5c79316a0066ef4e2387d74e46f93310db5
Adam Stokes <battlemidget@users.noreply.github.com>
2016-12-05 16:16:44 -0500

    dont prompt during install
    Signed-off-by: Adam Stokes <battlemidget@users.noreply.github.com>

--- lib/charmkit/plugins/nginx.rb
+++ lib/charmkit/plugins/nginx.rb
@@ -2,6 +2,7 @@ namespace :nginx do
   desc "Install NGINX"
   task :install do
     `apt-get update`
-    `apt-get install nginx-full`
+    `juju-log "Installing NGINX and its dependencies."`
+    `apt-get install -qyf nginx-full`
   end
 end
