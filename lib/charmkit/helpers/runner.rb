51c35691fd62c6ecd6ac0d26584ed010a52fdb18
Adam Stokes <battlemidget@users.noreply.github.com>
2016-12-06 15:07:08 -0500

    use tty-command again
    Signed-off-by: Adam Stokes <battlemidget@users.noreply.github.com>

--- lib/charmkit/helpers/runner.rb
+++ lib/charmkit/helpers/runner.rb
@@ -1,17 +1,10 @@
-require 'open3'
+require 'tty-command'
 
 module Charmkit
   module Helpers
     def run(*cmd)
-      stdout_s, stderr_s, status = Open3.capture3(*cmd)
-      if not status.success?
-        raise StandardError, "Failed to run command: #{stderr_s}"
-      end
-      return {
-        :out => stdout_s,
-        :err => stderr_s,
-        :status => status
-      }
+      runner = TTY::Command.new
+      return runner.run(*cmd)
     end
   end
 end
