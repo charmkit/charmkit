147ebfc353459438189edd7759dbcfdfd73afaa4
Adam Stokes <battlemidget@users.noreply.github.com>
2016-12-07 10:34:14 -0500

    fix nginx plugin
    Signed-off-by: Adam Stokes <battlemidget@users.noreply.github.com>

--- lib/charmkit/plugins/php.rb
+++ lib/charmkit/plugins/php.rb
@@ -12,8 +12,8 @@ namespace :php do
       'php-mbstring',
       'php-xml'
     ]
-    cmd.run "apt-get", "update"
-    cmd.run "juju-log", "Installing PHP7 and its dependencies."
-    cmd.run "apt-get", "install", "-qyf", pkgs.join(' ')
+    cmd.run "apt-get update"
+    cmd.run "juju-log Installing PHP7 and its dependencies."
+    cmd.run "apt-get install -qyf #{pkgs.join(' ')}"
   end
 end
--- lib/charmkit/version.rb
+++ lib/charmkit/version.rb
@@ -6,7 +6,7 @@ module Charmkit
   module VERSION
     MAJOR = 0
     MINOR = 4
-    PATCH = 5
+    PATCH = 6
     PRE = nil
 
     STRING = [MAJOR, MINOR, PATCH, PRE].compact.join('.')
