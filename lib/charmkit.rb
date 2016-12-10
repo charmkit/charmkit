992f0a723f6b09485c58bfcf3c4286da3922f603
Adam Stokes <battlemidget@users.noreply.github.com>
2016-12-10 13:45:55 -0500

    update helpers
    Signed-off-by: Adam Stokes <battlemidget@users.noreply.github.com>

--- lib/charmkit.rb
+++ lib/charmkit.rb
@@ -1,6 +1,8 @@
 require "charmkit/version"
 require "charmkit/helpers/template"
 require "charmkit/helpers/runner"
+require "charmkit/helpers/hookenv"
+require "charmkit/helpers/fs"
 
 module Charmkit; end
 extend Charmkit::Helpers
