53af7744c576eacfd1b4aede0ca31d92c126237d
Adam Stokes <battlemidget@users.noreply.github.com>
2016-12-21 08:59:06 -0500

    add crypt helper
    Signed-off-by: Adam Stokes <battlemidget@users.noreply.github.com>

--- lib/charmkit.rb
+++ lib/charmkit.rb
@@ -3,6 +3,7 @@ require "charmkit/helpers/template"
 require "charmkit/helpers/runner"
 require "charmkit/helpers/hookenv"
 require "charmkit/helpers/fs"
+require "charmkit/helpers/crypt"
 
 module Charmkit; end
 extend Charmkit::Helpers
--- lib/charmkit/version.rb
+++ lib/charmkit/version.rb
@@ -5,8 +5,8 @@ module Charmkit
 
   module VERSION
     MAJOR = 0
-    MINOR = 4
-    PATCH = 9
+    MINOR = 5
+    PATCH = 0
     PRE = nil
 
     STRING = [MAJOR, MINOR, PATCH, PRE].compact.join('.')
