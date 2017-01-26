9dc6a1acb34c8c0bf63977e68b5c60ab74242b87
Adam Stokes <battlemidget@users.noreply.github.com>
2017-01-25 22:56:11 -0500

    no idea
    Signed-off-by: Adam Stokes <battlemidget@users.noreply.github.com>

--- lib/charmkit/scroll.rb
+++ lib/charmkit/scroll.rb
@@ -2,8 +2,6 @@ module Charmkit
   class Scroll
     class << self
       include Helpers
-    end
-    module ScrollBase
       # Apt packages that the scroll requires
       #
       # @param [String] pkg Name of apt package
@@ -12,6 +10,5 @@ module Charmkit
       end
 
     end
-    extend ScrollBase
   end
 end
